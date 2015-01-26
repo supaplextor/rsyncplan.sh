4500b78240ad7156f95b59f2df4671d29257901d
Scott Edwards <supadupa@gmail.com>
2015-01-26 05:52:00 -0800

    ready to cleanup excess quoting in ${qm_FOO} elements.

--- tests/build-vmguest-test-rsyncplan.pl
+++ tests/build-vmguest-test-rsyncplan.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 
-# (C) Scott Edwards http://www.synapticmhz.com/l/via=build-vmguest-test-rsyncplan.sh/about.html
+# (C) Scott Edwards 2015 - q+d guest vm gen
+# About: http://www.synapticmhz.com/l/via=build-vmguest-test-rsyncplan.sh/about.html
 #
 # TODO: Be less locally significant to the dev host. Let me run $anywhere. This can be tested as below (eventually). Wow, take that recursion!
 # 
@@ -52,9 +53,15 @@ sub dirname {
 	return $return;
 }
 
+# make sure install iso is here.
+# download if need be.
+
 if (-s $local_isos) {
-	print "Found $local_isos\n";
+	print qq(Found "$local_isos"\n);
+	my $args = join(" " => $local_isos);
+	system ("sha256sum",$args);
 } else {
+	print qq(Downloading "$local_isos" (checking sha256sums).\n);
 	local $@ = '';
 # FIXME: use filehandle
 
@@ -72,13 +79,50 @@ if (-s $local_isos) {
 		my $url		= dirname($remote_sums{$distro})."/$uri_right";
 		warn		"url is $url";
 
-		system join("", "aria2c", " -x5 ",
-			"--auto-file-renaming=false ",
-			" -d "	=> quotemeta(`pwd | tr -d "\n"`),
-			" -o "	=> quotemeta($local_isos),
-			" " 	=> quotemeta($url));
+		system ("wget",
+		join(" " => quotemeta($url), "-O" => quotemeta($local_isos))
+		);
 
 		last;
 	}
 	close S;
 }
+
+my $GUEST_NAME		= "clean-debian-stable-$$";
+my $GUEST_RAM		= "500mb";
+
+my $VBoxManage		= "VBoxManage";
+  $VBoxManage		= "echo";
+
+my $qm_GUEST_NAME	= quotemeta $GUEST_NAME;
+my $qm_GUEST_RAM	= quotemeta $GUEST_RAM;
+
+sub createvm { return system ($VBoxManage, createvm => @_) };
+sub modifyvm { return system ($VBoxManage, modifyvm => @_) };
+
+createvm qq(--name "${qm_GUEST_NAME}" --register);
+modifyvm qq("${qm_GUEST_NAME}" --memory ${qm_GUEST_RAM} --acpi on --boot1 dvd);
+modifyvm qq("${qm_GUEST_NAME}" --ostype Debian);
+modifyvm qq("${qm_GUEST_NAME}" --nic1 bridged --bridgeadapter1 eth0);
+modifyvm qq("${qm_GUEST_NAME}" --nictype1 virtio);
+#       modifyvm "${qm_GUEST_NAME}" --macaddress1 XXXXXXXXXXXX
+
+##### DISK #####
+my $SATA="SATA Controller";
+my $VMs="~/VirtualBox VMs/";
+
+sub createhd		{ return system ($VBoxManage, createhd		=> @_) };
+sub storagectl		{ return system ($VBoxManage, storagectl	=> @_) };
+sub storageattach	{ return system ($VBoxManage, storageattach	=> @_) };
+sub unregistervm	{ return system ($VBoxManage, unregistervm	=> @_) };
+
+createhd qq(--filename "${VMs}${qm_GUEST_NAME}/${qm_GUEST_NAME}_sda.vdi" --size 10000);
+storagectl qq("${qm_GUEST_NAME}" --name "$SATA" --add sata);
+storageattach qq("${qm_GUEST_NAME}" --storagectl "$SATA" --port 0 --device 0 --type hdd --medium "${VMs}${qm_GUEST_NAME}/${qm_GUEST_NAME}_sda.vdi");
+storageattach qq("${qm_GUEST_NAME}" --storagectl "$SATA" --port 1 --device 0 --type dvddrive --medium ).quotemeta($local_isos);
+
+unregistervm qq("${qm_GUEST_NAME}" --delete);
+
+
+
+
