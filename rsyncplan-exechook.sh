199300f03dbc9eca77d323bf10ba94de39e59f24
Scott C Edwards <supadupa@gmail.com>
2022-11-01 02:22:12 -0700

    just about there

--- rsyncplan-exechook.sh
+++ rsyncplan-exechook.sh
@@ -2,24 +2,29 @@
 # (C) 2014-2020 - supaplextor cloned/exported code to https://github.com/supaplextor/rsyncplan
 # (C) 2014 Scott Edwards - https://code.google.com/p/rsyncplan/
 
-# tmpd=$(mktemp -d)
-# echo "$0 $@" >> ${tmpd}/bash-shim-args.$$
 
-shift $(($#-1))
-OFS=$1
+function outputFileSystem() {
+	shift $(($#-1))
+	echo "$1"
+}
+function newestSnapshots() {
+	set -x
+	pushd $1
+	ls -1d ????-??-??*/ 2>/dev/null |\
+		sort -nr |\
+		head -n 21 |\
+		tail -n 20 |\
+		sed -e s/^/--link-dest:/g |\
+		tr : =
+	popd
+	set +x
+}
 
 TIMESTAMP_YMDHMS=$(date "+%Y-%m-%d_%H%M%S")
+OFS=$(outputFileSystem $@)
+OFS_TS=$(mktemp -d "${OFS}${TIMESTAMP_YMDHMS}-XXXXXXXX")/
+links=$(newestSnapshots ${OFS} )
 
-links=$(ls -1d "${OFS}/????-??-??*/" 2>/dev/null |\
-	sort -nr |\
-	head -n 20 |\
-	awk '{print "--link-dest=__OFS__/"$1"/"}' |\
-	sed -e "s#__OFS__##g" | tr -s /
-)
-
-OFS_TS=$(mktemp -d "${OFS}/${TIMESTAMP_YMDHMS}-XXXXXXXX")
-test -d "${OFS_TS}" ||  mkdir -pv "${OFS_TS}" >&2
-
-echo rsync "${links}" "$@" "${OFS_TS}"
+echo rsync "${links}" "$@" "${OFS_TS}" >&2
 
 rmdir "${OFS_TS}" &>/dev/null
